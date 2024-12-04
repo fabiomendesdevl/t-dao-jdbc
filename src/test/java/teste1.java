5161bee91bce9e5d066523a29f2300840c4e5a19
Fabio Mendes <fabiomendesdev@outlook.com>
2024-12-03 21:03:51 -0300

    findById implementation

--- src/test/java/teste1.java
+++ src/test/java/teste1.java
@@ -10,15 +10,18 @@ import java.util.Scanner;
 
 public class teste1 {
     public static void main(String[] args) throws ParseException {
-        Department dp = new Department(2, "Fabio");
+       // Department dp = new Department(2, "Fabio");
         //System.out.println(dp);
 
 //        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 //        Date dt = sdf.parse(sc.next("10/10/2000"));
 
-        Seller sl = new Seller(1, "vendedor1", "fabio@gmail.com", new Date(), 1500.0, dp);
-        System.out.println(sl);
+//        Seller sl = new Seller(1, "vendedor1", "fabio@gmail.com", new Date(), 1500.0, dp);
+//        System.out.println(sl);
 
         SellerDao sellerDao  = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
